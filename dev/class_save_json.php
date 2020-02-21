4b8951cc705e2d97b8845daa86fe145e13ffc9c0
Максим-work <berez_max@mail.ru>
2020-02-21 16:50:27 +0300

    check request not complete

--- dev/class_save_json.php
+++ dev/class_save_json.php
@@ -1,7 +1,7 @@
 <?php
 class SaveAndLog {
     public $log_file;
-    protected $xml_content;
+    public $xml_content;
     public function __construct($log_file) {
     $this->log_file = $log_file;
     $this->xml_content = null;
@@ -9,7 +9,6 @@ class SaveAndLog {
     public function open_xml_log() {
         if (file_exists($this->log_file)) {
             $this->xml_content = simplexml_load_file($this->log_file);
-         
             print_r($this->xml_content);
             print_r('<h3>Содержимое лог-файла успешно прочитано!</h3>');
         } else {
@@ -17,13 +16,41 @@ class SaveAndLog {
         }
     }
     public function check_the_current_request($ETag) {
-        if ((string) $this->xml_content->year[-1]['current_year'] == '2020') {
-            echo "Сравнение выполнено";
-            // $new_day = $this->xml_content->addChild('year');//добавление нового года
-            // print_r($this->xml_content);
-            // $new_day->addAttribute('current_year', date('Y'));//установка аттрибута нового дня
-            // print_r($this->xml_content);
+        if (!$this->xml_content->xpath("//year[@current_year='2021']")) {//FIXME:date('Y')
+            echo "<p><u>Дописывание года выполняется:</u></p>";
+            $new_year = $this->xml_content->addChild('year');//добавление нового года
+            $new_year->addAttribute('current_year', "2021");//добавление аттрибута нового года ВАЖНО ->
+            $new_month = $new_year->addChild('month');//добавление нового месяца
+            $new_month->addAttribute('current_month', date('m'));
+            $new_day = $new_month->addChild('day');//добавление нового дня
+            $new_day->addAttribute('current_day', date('d'));
+            print_r ($this->xml_content);
+            file_put_contents($this->log_file, $this->xml_content->asXML());//save file
         }
+        $verifiable_month = "//year[@current_year='2021']/month[@current_month='04']";
+        if (!$this->xml_content->xpath($verifiable_month)) {//TODO:Сократить ветви, запись вынести отдельно
+            echo "<p><u>Дописывание месяца выполняется:</u></p>";
+            $new_month = $this->xml_content->xpath("//year[@current_year='2021']")->addChild('month');
+            $new_month->addAttribute('current_month', date('04'));
+            $new_day = $new_month->addChild('day');//добавление нового дня
+            $new_day->addAttribute('current_day', date('d'));
+            print_r ($this->xml_content);
+            file_put_contents($this->log_file, $this->xml_content->asXML());//save file
+        }
+        // $request = $this->xml_content->xpath("//day/request[@filename='Xml_1']");
+        // print_r ($request);
+        // echo '<br>';
+        // print_r($this->xml_content);
+        // $this->open_xml_log();
+        // echo '<br>';
+        // print_r($this->xml_content);
+        // if (((string) $this->xml_content->year[-1]->month[-1])) {
+        //     echo "<p><u>Проверка месяца выполнена</u></p>";
+        //     $new_month = $this->xml_content->year[-1]->addChild('month');//добавление нового года
+        //     $new_month->addAttribute('current_month', "03");//добавление аттрибута нового месяца ВАЖНО ->
+        //     print_r ($this->xml_content);
+        //     file_put_contents($this->log_file, $this->xml_content->asXML());//save file
+        // }
         // if ($this->xml_content->year[-1]->month[-1]->day[-1]->request['filename']) {
         //     if ($this->xml_content->year[-1]->month[-1]->day[-1]->request[-1] != $ETag) {//json есть и обновился
         //         // TODO:Добавить сохранить новый json
