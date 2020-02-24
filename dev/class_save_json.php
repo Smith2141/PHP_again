0a2d660d5172552fbb0bbc75e7a77852f38cee8d
smith_mint <berez_max@mail.ru>
2020-02-24 17:39:15 +0300

    create three funcs

--- dev/class_save_json.php
+++ dev/class_save_json.php
@@ -8,22 +8,28 @@ class SaveAndLog {
     }
     public function open_xml_log() {
         if (file_exists($this->log_file)) {
-            $this->xml_content = simplexml_load_file($this->log_file);
+            // $this->xml_content = simplexml_load_file($this->log_file);
+            $this->xml_content = new SimpleXMLElement($this->log_file, NULL, TRUE);
             print_r($this->xml_content);
             print_r('<h3>Содержимое лог-файла успешно прочитано!</h3>');
         } else {
             exit("Не удалось открыть файл $this->log_file.");
         }
     }
-    public function check_the_current_request($ETag) {
+    public function check_the_current_year() {
         $current_year = date('Y');
         $verifiable_year = $this->xml_content->xpath("//year[@current_year=$current_year]");
         if (!$verifiable_year) {//FIXME:date('Y')
             echo "<p><u>Дописывание года выполняется:</u></p>";
             $new_year = $this->xml_content->addChild('year');//добавление нового года
             $new_year->addAttribute('current_year', date('Y'));//добавление аттрибута нового года ВАЖНО ->
+            print_r ($this->xml_content);
         }
+    }
+    public function check_the_current_month() {
+        $current_year = date('Y');
         $current_month = date('Ym');
+        $verifiable_year = $this->xml_content->xpath("//year[@current_year=$current_year]");
         $verifiable_month = $this->xml_content->xpath("//month[@current_month=$current_month]");
         if (!$verifiable_month) {//TODO:Сократить ветви, запись вынести отдельно
             echo "<p><u>Дописывание месяца выполняется:</u></p>";
@@ -31,10 +37,15 @@ class SaveAndLog {
             foreach ($new_year as $year) {
                 if ($verifiable_year) {
                     $year->addChild('month')->addAttribute('current_month', date('Ym'));
+                    print_r ($this->xml_content);
                 }
             }
         }
+    }
+    public function check_the_current_day() {
+        $current_month = date('Ym');
         $current_day = date('Ymd');
+        $verifiable_month = $this->xml_content->xpath("//month[@current_month=$current_month]");
         $verifiable_day = $this->xml_content->xpath("//day[@current_day=$current_day]");
         if (!$verifiable_day) {
             echo "<p><u>Дописывание дня выполняется:</u></p>";
@@ -42,17 +53,27 @@ class SaveAndLog {
             foreach ($new_month as $month) {
                 if ($verifiable_month) {
                     $month->addChild('day')->addAttribute('current_day', date('Ymd'));
+                    print_r ($this->xml_content);
+                    file_put_contents($this->log_file, $this->xml_content->asXML());//save file
+                    return 'current day added';
                 }
             }
+    
         }
-        print_r ($this->xml_content);
-        file_put_contents($this->log_file, $this->xml_content->asXML());//save file
     }
+
 }
 
 $ETag = '5e43f25b-1c44';
 $log_path = '../json_log2.xml';//FIXME:Путь к лог-файлу 
 $test_obj = new SaveAndLog($log_path);
 $test_obj->open_xml_log();
-$test_obj->check_the_current_request($ETag);
+$test_obj->check_the_current_year();
+$test_obj->check_the_current_month();
+$a = $test_obj->check_the_current_day();
+if ($a == 'current day added') {
+    echo '<h1>working</h1>';
+} else {
+    echo '<h1>silent</h1>';
+}
 ?>
